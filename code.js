7ed10c10e722792f6dfa2f937e5d118a92cd6b5b
gonzalo masa <gonzalo-masa@outlook.com>
2022-12-19 01:27:58 -0300

    Primer Project

--- code.js
+++ code.js
@@ -1,40 +1,73 @@
 function choiceComputer() {
-    let choice = ["Piedra", "Papel", "Tijera"];
-    let indice = Math.floor(Math.random() * choice.length);
-    return choice[indice];
-  }
-  /* let choice = choiceComputer()
+  let choice = ["Piedra", "Papel", "Tijera"];
+  let indice = Math.floor(Math.random() * choice.length);
+  return choice[indice];
+}
+/* let choice = choiceComputer()
   console.log(choice) */
-  
-  function oneRound(playerSelection, computerSelection) {
+
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-  
-    if (playerSelection === computerSelection) {
-      return "Empate!";
-    } else if (playerSelection === "piedra") {
-      if (computerSelection === "tijeras") {
-        return "Ganas! Piedra vence a tijeras.";
-      } else {
-        return "Pierdes! Papel vence a piedra.";
-      }
-    } else if (playerSelection === "papel") {
-      if (computerSelection === "piedra") {
-        return "Ganas! Papel vence a piedra.";
-      } else {
-        return "Pierdes! Tijeras vencen a papel.";
-      }
-    } else if (playerSelection === "tijeras") {
-      if (computerSelection === "papel") {
-        return "Ganas! Tijeras vencen a papel.";
-      } else {
-        return "Pierdes! Piedra vence a tijeras.";
-      }
+
+  if (playerSelection === computerSelection) {
+    return "Empate!";
+  } else if (playerSelection === "piedra") {
+    if (computerSelection === "tijeras") {
+      return "Ganas! Piedra vence a tijeras.";
     } else {
-      return "Entrada inválida";
+      return "Pierdes! Papel vence a piedra.";
     }
+  } else if (playerSelection === "papel") {
+    if (computerSelection === "piedra") {
+      return "Ganas! Papel vence a piedra.";
+    } else {
+      return "Pierdes! Tijeras vencen a papel.";
+    }
+  } else if (playerSelection === "tijeras") {
+    if (computerSelection === "papel") {
+      return "Ganas! Tijeras vencen a papel.";
+    } else {
+      return "Pierdes! Piedra vence a tijeras.";
+    }
+  } else {
+    return "No existe";
   }
-  
-  const playerSelection = "piedra";
+}
+
+/*   const playerSelection = "piedra";
   const computerSelection = choiceComputer();
-  console.log(oneRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));  */
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Elige piedra, papel o tijeras:");
+    let computerSelection = choiceComputer();
+    let result = playRound(playerSelection, computerSelection);
+
+    if (result.includes("Ganas")) {
+      playerScore++;
+    } else if (result.includes("Pierdes")) {
+      computerScore++;
+    }
+
+    console.log(result);
+  }
+
+  if (playerScore > computerScore) {
+    console.log(
+      "¡Ganas la partida con un total de " + playerScore + " puntos!"
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      "Pierdes la partida con un total de " + playerScore + " puntos."
+    );
+  } else {
+    console.log("¡Empate! Ambos tienen " + playerScore + " puntos.");
+  }
+}
+
+game();
