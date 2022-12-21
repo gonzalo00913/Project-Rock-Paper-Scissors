2bea11c1da0837ef789b3d482e0a4dc1a8934a88
gonzalo masa <gonzalo-masa@outlook.com>
2022-12-20 21:17:18 -0300

    nueva rama, nuevo cambios

--- code.js
+++ code.js
@@ -1,3 +1,52 @@
+/* const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'Esta es mi primera vez no entiendo nada'
+container.appendChild(content);
+
+const textoRed = document.getElementById('texto-red')
+textoRed.innerHTML = 'soy rojo!'
+textoRed.style.color = "red"
+
+
+const textoAzul = document.getElementById('soy-azul');
+textoAzul.innerHTML = 'soy azul!'
+textoAzul.style.color = 'blue'
+
+const containerOne= document.createElement('div');
+const header = document.createElement('h1');
+header.innerHTML = 'Estoy dentro de un Div';
+containerOne.style.border = '1px solid black';
+containerOne.appendChild(header)
+document.body.appendChild(containerOne);
+const parrafo = document.createElement('p');
+parrafo.innerHTML = 'Yo tambien';
+
+parrafo.style.backgroundColor = 'pink'
+containerOne.appendChild(parrafo);
+document.body.appendChild(containerOne)
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert('Hello World!') 
+})
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+}); */
+
+/* <script>
+  const container = document.createElement('div');
+  const header = document.createElement('h1');
+  header.innerHTML = 'estoy en un div';
+  container.style.border = '1px solid black';
+  container.style.backgroundColor = 'pink';
+  container.appendChild(header);
+  document.body.appendChild(container);
+</script> */
+
+
 function choiceComputer() {
   let choice = ["Piedra", "Papel", "Tijera"];
   let indice = Math.floor(Math.random() * choice.length);
@@ -14,24 +63,24 @@ function playRound(playerSelection, computerSelection) {
     return "Empate!";
   } else if (playerSelection === "piedra") {
     if (computerSelection === "tijeras") {
-      return "Ganas! Piedra vence a tijeras.";
+      return "Ganaste! Piedra vence a tijeras.";
     } else {
-      return "Pierdes! Papel vence a piedra.";
+      return "Perdiste! Papel vence a piedra.";
     }
   } else if (playerSelection === "papel") {
     if (computerSelection === "piedra") {
-      return "Ganas! Papel vence a piedra.";
+      return "Ganaste! Papel Vence a piedra.";
     } else {
-      return "Pierdes! Tijeras vencen a papel.";
+      return "Perdiste! Tijeras vence a papel.";
     }
   } else if (playerSelection === "tijeras") {
     if (computerSelection === "papel") {
-      return "Ganas! Tijeras vencen a papel.";
+      return "Ganaste! Tijeras vence a papel.";
     } else {
-      return "Pierdes! Piedra vence a tijeras.";
+      return "anda pa ya BOBO!! Piedra vence a tijeras.";
     }
   } else {
-    return "No existe";
+    return "anda pa ya BOBO!";
   }
 }
 
@@ -44,13 +93,13 @@ function game() {
   let computerScore = 0;
 
   for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Elige piedra, papel o tijeras:");
+    let playerSelection = prompt("Elegi piedra, papel o tijeras:");
     let computerSelection = choiceComputer();
     let result = playRound(playerSelection, computerSelection);
 
-    if (result.includes("Ganas")) {
+    if (result.includes("Ganaste ;)")) {
       playerScore++;
-    } else if (result.includes("Pierdes")) {
+    } else if (result.includes("Perdiste JAJAJA")) {
       computerScore++;
     }
 
@@ -59,15 +108,59 @@ function game() {
 
   if (playerScore > computerScore) {
     console.log(
-      "¡Ganas la partida con un total de " + playerScore + " puntos!"
+      "¡Ganaste la partida con un total de " + playerScore + " puntos!"
     );
   } else if (playerScore < computerScore) {
     console.log(
-      "Pierdes la partida con un total de " + playerScore + " puntos."
+      "Pecheaste la partida con un total de " + playerScore + " puntos."
     );
   } else {
-    console.log("¡Empate! Ambos tienen " + playerScore + " puntos.");
+    console.log("¡Aburrido! Ambos tienen " + playerScore + " puntos.");
   }
 }
 
-game();
+//game();
+
+
+//creando div.
+var resultadosDiv = document.createElement('div');
+//agregando div al dom
+document.body.appendChild(resultadosDiv);
+
+//creando botones.
+var buttonRock = document.createElement('button')
+var buttonPaper = document.createElement('button')
+var buttonScissors = document.createElement('button')
+
+//este es el texto de cada boton.
+buttonRock.textContent = "piedra";
+buttonPaper.textContent = "papel";
+buttonScissors.textContent = "tijeras";
+
+//agregando eventos a los botones.
+buttonRock.addEventListener('click', function(){
+var resultado = playRound("Piedra", choiceComputer());
+resultadosDiv.textContent = resultado
+});
+
+buttonPaper.addEventListener('click', function(){
+  var resultado = playRound("Piedra", choiceComputer());
+  resultadosDiv.textContent = resultado
+});
+
+
+buttonScissors.addEventListener('click', function(){
+var resultado = playRound("Tijeras", choiceComputer());
+resultadosDiv.textContent = resultado
+});
+
+//agregando al dom.
+
+document.body.appendChild(buttonRock);
+document.body.appendChild(buttonPaper);
+document.body.appendChild(buttonScissors);
+
+
+//esto es para mostrar los resultados en un div.
+
+
