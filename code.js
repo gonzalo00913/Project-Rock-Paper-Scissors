63f73821247498803759ac6df3f0d5b297ae1f82
gonzalo masa <gonzalo-masa@outlook.com>
2022-12-28 09:03:53 -0300

    cambios de la nueva rama

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+
+let playerScore = 0;
+let computerScore = 0;
+let totalDePuntos = 0;
+
 function choiceComputer() {
   let choice = ["Piedra", "Papel", "Tijeras"];
   let indice = Math.floor(Math.random() * choice.length);
@@ -5,56 +10,100 @@ function choiceComputer() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  let result;
   if (playerSelection === computerSelection) {
-    return "Empate!";
+    result = "Empate!";
   } else if (playerSelection === "Piedra") {
-    if (computerSelection === "tijeras") {
-      return "Gana! Piedra vence a tijeras.";
+    if (computerSelection === "Tijeras") {
+      result = "¡Ganaste!";
+      playerScore++;
     } else {
-      return "Pierde! Papel vence a piedra.";
+      result = "!Pierdes!";
+      computerScore++
     }
   } else if (playerSelection === "Papel") {
-    if (computerSelection === "piedra") {
-      return "Gana! Papel Vence a piedra.";
+    if (computerSelection === "Piedra") {
+      result = "¡Ganaste!";
+      playerScore++;
     } else {
-      return "Pierde! Tijeras vence a papel.";
+      result = "!Pierdes!";
+      computerScore++
     }
   } else if (playerSelection === "Tijeras") {
-    if (computerSelection === "papel") {
-      return "Gana! Tijeras vence a papel.";
+    if (computerSelection === "Papel") {
+      result = "¡Ganaste!";
+      playerScore++;
     } else {
-      return "Pierde! Piedra vence a tijeras.";
+      result = "!Pierdes!";
+      computerScore++
     }
   } else {
-    return "Selección inválida";
+    result = "Selección inválida";
   }
+  return {result,playerScore, computerScore, totalDePuntos};
 }
 
 const botonRock = document.getElementById("rock");
 const botonPapel = document.getElementById("papel");
 const botonTijeras = document.getElementById("tijeras");
 const puntosText = document.getElementById("texto-inicio");
+const textoComputadora = document.getElementById("texto-computadora");
+const textoPlayer = document.getElementById("texto-player");
+
+const totalPuntos = document.getElementById("total-puntos")
+
+let round = 1;
 
 botonRock.addEventListener("click", function () {
-  const computerChoice = choiceComputer();
-  const result = playRound("Piedra", computerChoice);
-  const puntosText = document.querySelector("#puntos #texto-inicio");
-  puntosText.textContent = `Elegiste Piedra y la computadora eligió ${computerChoice}. ${result}`;
+  if (round <= 10) {
+    const computerChoice = choiceComputer();
+    const { result} = playRound("Piedra", computerChoice);
+    puntosText.textContent = `${result}`;
+    textoComputadora.textContent = `Puntos Computadora = ${computerScore}`
+    textoPlayer.textContent = `Tus Puntos =  ${playerScore}`
+    round++;
+  } else {
+    puntosText.textContent = `Fin del juego.`;
+    round = 1;
+    playerScore = 0;
+    computerScore = 0;
+  }
 });
 
 botonPapel.addEventListener("click", function () {
-  const computerChoice = choiceComputer();
-  const result = playRound("Papel", computerChoice);
-  const puntosText = document.querySelector("#puntos #texto-inicio");
-  puntosText.textContent = `Elegiste Papel y la computadora eligió ${computerChoice}. ${result}`;
+  if (round <= 10) {
+    const computerChoice = choiceComputer();
+    const { result} = playRound("Papel", computerChoice);
+    puntosText.textContent = `${result}`;
+    textoComputadora.textContent = `Puntos Computadora = ${computerScore}`
+    textoPlayer.textContent = `Tus Puntos =  ${playerScore}`
+    round++;
+  } else {
+    puntosText.textContent = `Fin del juego.`;
+    round = 1;
+    playerScore = 0;
+    computerScore = 0;
+  }
 });
 
 botonTijeras.addEventListener("click", function () {
-  const computerChoice = choiceComputer();
-  const result = playRound("Tijeras", computerChoice);
-  const puntosText = document.querySelector("#puntos #texto-inicio");
-  puntosText.textContent = `Elegiste Tijeras y la computadora eligió ${computerChoice}. ${result}`;
+  if (round <= 10) {
+    const computerChoice = choiceComputer();
+    const { result} = playRound("Tijeras", computerChoice);
+    puntosText.textContent = `${result}`;
+    textoComputadora.textContent = `Puntos Computadora = ${computerScore}`
+    textoPlayer.textContent = `Tus Puntos =  ${playerScore}`
+    round++;
+  } else {
+    puntosText.textContent = `Fin del juego`;
+    round = 1;
+    playerScore = 0;
+    computerScore = 0;
+  }
 });
+
+
+//const puntosText = document.querySelector("#puntos #texto-inicio");
 
 /*  function game() {
   let playerScore = 0;
